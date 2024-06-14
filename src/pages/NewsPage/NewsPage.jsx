450a38143cdca95f3a7b5817e401e248c9a9b14c
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-06-14 11:21:18 -0300

    Pesquisa Tempo real

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import styles from './NewsPage.module.css'
 import Card from '../../components/Card/Card'
 import Container from 'react-bootstrap/Container'
+import PesquisaTempoReal from '../../components/Pesquisa/Pesquisa'
 
 const NewsPage = ({ noticias }) => {
     return (
         <>
             <Container fluid className={styles.newsPage}>
+                <PesquisaTempoReal></PesquisaTempoReal>
                 {noticias && noticias.map((noticia, i) => {
                     return (
                         <Card key={i} noticia={noticia}></Card>

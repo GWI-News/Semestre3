32097a607849b53dd9b3ea73fdf3aaad63840d19
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-06-17 20:43:25 -0300

    Criação de Dashboard e Rastreio de Eventos com Firebase Analitcs.

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -5,22 +5,33 @@ import { getStorage } from "firebase/storage"
 import { getAuth } from "firebase/auth"
 import credentials from "../../credentials.json"
 
-const firebaseConfig = {
-    apiKey: credentials.PROD_API.REACT_APP_PROD_API_KEY,
-    authDomain: credentials.PROD_API.REACT_APP_PROD_AUTH_DOMAIN,
-    projectId: credentials.PROD_API.REACT_APP_PROD_PROJECT_ID,
-    storageBucket: credentials.PROD_API.REACT_APP_PROD_STORAGE_BUCKET,
-    messagingSenderId: credentials.PROD_API.REACT_APP_PROD_MESSAGING_SENDER_ID,
-    appId: credentials.PROD_API.REACT_APP_PROD_APP_ID,
-    measurementId: credentials.PROD_API.REACT_APP_PROD_MEASUREMENT_ID
-};
+let firebaseConfig = {}
+const environment = window.location.hostname
+
+if (environment.includes('e715f')) {
+    firebaseConfig = {
+        apiKey: credentials.PROD_API.REACT_APP_PROD_API_KEY,
+        authDomain: credentials.PROD_API.REACT_APP_PROD_AUTH_DOMAIN,
+        projectId: credentials.PROD_API.REACT_APP_PROD_PROJECT_ID,
+        storageBucket: credentials.PROD_API.REACT_APP_PROD_STORAGE_BUCKET,
+        messagingSenderId: credentials.PROD_API.REACT_APP_PROD_MESSAGING_SENDER_ID,
+        appId: credentials.PROD_API.REACT_APP_PROD_APP_ID,
+        measurementId: credentials.PROD_API.REACT_APP_PROD_MEASUREMENT_ID
+    }
+} else {
+    firebaseConfig = {
+        apiKey: credentials.DEV_API.REACT_APP_DEV_API_KEY,
+        authDomain: credentials.DEV_API.REACT_APP_DEV_AUTH_DOMAIN,
+        projectId: credentials.DEV_API.REACT_APP_DEV_PROJECT_ID,
+        storageBucket: credentials.DEV_API.REACT_APP_DEV_STORAGE_BUCKET,
+        messagingSenderId: credentials.DEV_API.REACT_APP_DEV_MESSAGING_SENDER_ID,
+        appId: credentials.DEV_API.REACT_APP_DEV_APP_ID,
+        measurementId: credentials.DEV_API.REACT_APP_DEV_MEASUREMENT_ID
+    }
+}
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-const db = getFirestore(app)
-const storage = getStorage(app)
-const auth = getAuth(app)
-
-export {db}
-export {storage}
-export {auth}
+export const analytics = getAnalytics(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
+export const auth = getAuth(app)

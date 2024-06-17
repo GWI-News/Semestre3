073bf3ceda0d129b6884e9e80aeb226c25c71dbf
carloosramos <carlos.ramos6@fatec.sp.gov.br>
2024-06-17 20:58:05 -0300

    Criação de Página de Erro 404 Personalizada

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ import Faq from './pages/Faq/Faq';
 
 import Cotacoes from './components/Cotacoes/Cotacoes';
 import Crud from './pages/Crud/Crud';
+import PaginaErro from './pages/PaginaErro/PaginaErro';
 
 
 function App() {
@@ -90,7 +91,7 @@ function App() {
             <Route path='/Perfil/Adm' element={<PerfilAdm></PerfilAdm>}></Route>
             <Route path='/Perfil/Leitor' element={<PerfilLeitor></PerfilLeitor>}></Route>
             <Route path='/Faq' element={<Faq></Faq>}></Route>
-            <Route path='*' element={<Navigate to='/' />}></Route>
+            <Route path='*' element={<PaginaErro />}></Route>
 
             <Route path='/Economia/Moedas' element={<Cotacoes></Cotacoes>}></Route>
             <Route path='/Crud' element={<Crud></Crud>}></Route>

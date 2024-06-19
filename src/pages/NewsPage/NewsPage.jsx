8f995321d4d2b932fd852a4a56de8b4b25f9030f
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-06-19 10:26:23 -0300

    Correção de Bugs

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styles from './NewsPage.module.css'
 import { NavLink, useLocation } from 'react-router-dom'
 
-import { doc, getDoc, setDoc } from 'firebase/firestore'
+import { doc, getDoc, setDoc, getDocs, collection } from 'firebase/firestore'
 import { db } from '../../firebase/config'
 import { analytics } from '../../firebase/config'
 import { logEvent } from 'firebase/analytics'
@@ -13,11 +13,20 @@ import PesquisaTempoReal from '../../components/Pesquisa/Pesquisa'
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel'
 
-const NewsPage = ({ noticias, categoria }) => {
+const NewsPage = ({ categoria }) => {
   const location = useLocation()  
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [location])
+
+  const [noticias, setNoticias] = useState([])
+  useEffect(() => {
+    const getNoticias = async () => {
+      const data = await getDocs(collection(db, 'Noticias'))
+      setNoticias(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+    }
+    getNoticias()
+  }, [])
   
   useEffect(() => {
     const buscarDados = async () => {
@@ -67,8 +76,8 @@ const NewsPage = ({ noticias, categoria }) => {
                       src={carouselItem.img}
                       alt={carouselItem.alt_imagem}
                     />
-                    <Carousel.Caption className={styles.carouselTexto}>
-                      <h3>{carouselItem.titulo}</h3>
+                    <Carousel.Caption className={`${styles.carouselTexto} p-1`}>
+                      <h3 className='m-0'>{carouselItem.titulo}</h3>
                     </Carousel.Caption>
                   </NavLink>
                 </Carousel.Item>
